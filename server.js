ba2ed86e54e06823400cd8bc389e5b0f7427789c
chen <chen.duo@jp.panasonic.com>
2023-12-05 19:55:11 +0900

    For DB

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-const app = express();
 import morgan from 'morgan';
-import {nanoid} from 'nanoid';
-
+import mongoose from 'mongoose';
+import 'express-async-errors';
 //routers
-import jobRouter from './routes/jobRouter.js';
+import router from './routes/jobRouter.js';
 
-
+const app = express();
+app.use(express.json());
 //middleware
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', router);
+
+app.use(morgan('dev'));
 
 app.use('*', (req, res) =>{
   res.status(404).json({msg: 'not found'});
@@ -22,7 +24,14 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5100;
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`);
-});
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
 
